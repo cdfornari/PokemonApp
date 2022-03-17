80a6fc72d3a2f61d537314578335569726b4724b
Carlos Fornari <lilfornah@gmail.com>
2022-03-16 23:42:56 -0400

    fix favorites card

--- components/pokemon/FavoritePokemonCard.tsx
+++ components/pokemon/FavoritePokemonCard.tsx
@@ -14,7 +14,7 @@ export const FavoritePokemonCard = ({pokemonId}: Props) => {
         <Grid xs={6} sm={3} md={2} xl={1} key={pokemonId}>
             <Card hoverable clickable css={{padding: 10}} onClick={onClick}>
                 <Card.Image 
-                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${ pokemonId }.svg`}
+                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${ pokemonId }.png`}
                     width={'100%'}
                     height={140}
                 />
